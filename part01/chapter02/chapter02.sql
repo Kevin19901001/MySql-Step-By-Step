f8fe147d3a9914e147c14f30744263af6c0ea79f
kevin <huanqing19901001@163.com>
2023-08-27 21:29:01 +0800

    Add some new content.

--- part01/chapter02/chapter02.sql
+++ part01/chapter02/chapter02.sql
@@ -21,3 +21,45 @@ create table emp(
 
 // 查看表
 desc emp;
+
+show tables;
+
+// 5、修改表
+// 1、修改表字段
+create table emp(
+	ename		varchar(10),
+	hiredate	date,
+	sal			decimal(10, 2),
+	dept_no		int(2)
+);
+
+desc emp;
+
+alter table emp modify ename varchar(20);
+
+// 2、增加表字段
+alter table emp add age int(3);
+
+// 3、删除表字段
+alter table emp drop column age;
+
+desc emp;
+
+// 4、字段改名
+alter table emp change age age1 int(4);
+
+desc emp;
+
+// 5、修改字段排列顺序
+alter table emp add birth date after ename;
+
+desc emp;
+
+alter table emp modify age1 int(3) first;
+
+desc emp;
+
+// 6、变更表名
+alter table emp rename to emp1;
+
+show tables;
